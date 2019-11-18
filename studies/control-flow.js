307367e8224605110359ae2ac9f7103403cb38f5
Ubuntu <ubuntu@ip-172-31-44-52.ec2.internal>
2019-11-18 21:06:29 +0000

    updated studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -16,9 +16,12 @@ if (true) { // condition statement in ()
     console.log(true); // code block that will run if condition statement is met
 }
 
-// 2. Else Statement // The else statement runs a code block if all other previous conditional stateemtn are not truthy.
-// If none of the above conditional statements evaluate truthy, the else code block will run.
-// If statements are best used when an expression gives a truthy value.
+// 2. Else Statement // 
+/**
+ * The else statement runs a code block if all other previous conditional stateemtn are not truthy.
+ * If none of the above conditional statements evaluate truthy, the else code block will run.
+ * If statements are best used when an expression gives a truthy value.
+ */ 
 
 if (true) { // condition statement in ()
     console.log(true); // code block that will run if condition statement is met
@@ -26,8 +29,11 @@ if (true) { // condition statement in ()
     console.log(false); // code block that runs if condition statement is not met
 }
 
-// 3. Else-If Statement // The else-if statement tests if another condtion evaluates truthy.
-// If the first condition evaluates falsy but the else-if evaluates truty, the else-if code block will run.
+// 3. Else-If Statement //
+/**
+ * The else-if statement tests if another condtion evaluates truthy.
+ * If the first condition evaluates falsy but the else-if evaluates truty, the else-if code block will run.
+ */ 
 
 let x = 1;
 if ( x === 1) { // condition statement 1 in ()
@@ -38,11 +44,16 @@ if ( x === 1) { // condition statement 1 in ()
     console.log(false); // code block that runs if both condition statements are not met
 }
 
-// 4. Switch Statement // The swithch statement tests to see if an expression meets any of the cases within the switch statement.
-// If the expression meet one of the cases, that case's code block will run.
-// If the expression meets none of the cases, the defalult code block will run (similar to else statement).
-// It is better to use a swithch statement if you are comparing multiple cases for a given expression. 
-// Example: var x could be a value between 0 and 9, so switch can have nine or more cases.
+// 4. Switch Statement //
+/**The swithch statement tests to see if an expression meets any of the cases within the switch statement.
+ * Cases are similar to if statements. They are compared to the given expression. 
+ * If the expression meet one of the cases, that case's code block will run.
+ * In switch statements, break follows a code block of a case and makes the code stop a specified case matches the expression.
+ * Break prevents the code from continuing, similar to code blocks in if statements.
+ * If the expression meets none of the cases, the defalult code block will run (similar to else statement).
+ * It is better to use a swithch statement if you are comparing multiple cases for a given expression. 
+ * Example: var x could be a value between 0 and 9, so switch can have nine or more cases.
+ */
 
 switch (x) {
     case 1: // expression being compared to case 1
--- studies/datatypes.js
+++ studies/datatypes.js
@@ -8,29 +8,77 @@
  * 
  */
 
-// 1. Primitive Data Types // Can be accessed by assigning to a variable or typing data when needed
-- Number;  1, 3, 500, 7.01; let num = 1;// any number value, if number 1 is needed, type 1 wherever 1 is required or assign 1 to a variable
-- String; "hi"; '73#$'; // any characters typed between single quotes ('') or double quotes("").
-- Boolean; true; false;// true or false 
-- undefined; var stuff; console.log(stuff); // a value that is not defined, stuff console logged will be undefined
-- null; let x = null; // no value at all, console logged x will be null
-- NaN; let y = NaN; // number type that is "not a number", console logged y will be NaN
-- Infinity; stuff = Infinity; // number value of infinity, stuff console logged will be a value of Infinity
-- -Infinity; stuff = -Infinity; // number value of negative infinity, stuff console logged will be a value of -Infinity
+// 1. Primitive Data Types // 
+// Primitive Data Types can be accessed by assigning to a variable or typing data when needed
 
-// 2. Complex Data Types // Can be accessed by assigning to a variable or typing data when needed
-- Array; [1, "2", true, {one: 1}, function so(){ console.log("so")}];// high level lists of data that can contain primitve and complex data
-- Object; {one: 1}// a container of key/value(primitive or complex data) pairs of data
-- Function; function so(){ console.log("so")}// a sub program that performs a task when invoked
+// a. Number //
+// any number value, if number 1 is needed, type 1 wherever 1 is required or assign 1 to a variable
+- Number;  1, 3, 500, 7.01; let num = 1;
 
-// 3. Copy by Value // primitive data types are copied to varialbles with = sign
-// Different variables can be assigned the same data type of another variable with = sign
+// b. String //
+// any characters typed between single quotes ('') or double quotes("").
+ let string = "hi"; let otherString = '73#$'; 
+
+// c. Boolean //
+// true or false 
+- Boolean; true; false; 
+
+// d. Undefined //
+// a value that is not defined, stuff console logged will be undefined
+- undefined; var stuff; console.log(stuff); 
+
+// e. Null //
+// no value at all, console logged x will be null
+- null; let x = null; 
+
+// f. NaN //
+// number type that is "not a number", console logged y will be NaN
+- NaN; let y = NaN; 
+
+// g. Infinity //
+// number value of infinity, stuff console logged will be a value of Infinity
+- Infinity; stuff = Infinity; 
+
+// h. Negative Infinity //
+// number value of negative infinity, stuff console logged will be a value of -Infinity
+- -Infinity; stuff = -Infinity; 
+
+// 2. Complex Data Types // 
+/**
+ * high level lists of data that can contain primitve and complex data
+ * Can be accessed by assigning to a variable or typing data when needed
+ */ 
+
+// a. Array //
+// A collection of values that have an indexed order
+ [1, "2", true, {one: 1}, function hey(){ console.log("hey")}]; 
+ 
+// b. Object //
+// a container of key/value(primitive or complex data) pairs of data
+ {one: 1} 
+ 
+ // c. Function //
+// a sub program that performs a task when invoked
+ function so(){ console.log("so")}
+
+// 3. Copy by Value // 
+/**
+ * Primitive Data Types take up very small amounts of memory.
+ * primitive data types are copied to varialbles with = sign
+ * Different variables can be assigned the same data type of another variable with = sign
+ */ 
+ 
 let greet = "hello"; // primitive data assigned to varialbe
 let holla = greet; // "hello" copied to variable holla from greet 
 console.log(greet); // prints => 'hello' 
 console.log(holla);  // prints => 'hello' copied value into variable holla
 
-// 4. Copy by Reference // complex data types are copied by reference with the = sign
+// 4. Copy by Reference // 
+/**
+ * Complex Data Types take up far more memory than primitive.
+ * Complex data types are copied by reference with the = sign
+ */
+ 
 let arr = [1, "2", true, [3, false], {x:8}]; // complex data assigned to varialbe
 let newArr = arr; // the array arr has been copied to newArr
 console.log(newArr[3]); // the nested array in arr can be accessed by referencing 4th element
